3f1bc51b2a165cb014305388923455e0b7036744
Antonius Blantran de Rozari <derozari@gmail.com>
2022-11-11 23:14:13 +0700

    First upload starter file

---
+++ modules/components.py
@@ -0,0 +1,18 @@
+import os
+
+import tensorflow as tf
+import tensorflow_model_analysis as tfma
+
+
+
+def init_components(
+    data_dir,
+    transform_module,
+    training_module,
+    training_steps,
+    eval_steps,
+    serving_model,
+):
+
+
+    return components
