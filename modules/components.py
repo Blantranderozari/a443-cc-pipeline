4195865a8531ea5d495cbe454b648864e082207f
Antonius Blantran de Rozari <derozari@gmail.com>
2022-11-12 13:05:06 +0700

    Add StatisticGen, SchemaGen, Example Validator, Transform and Trainer

--- modules/components.py
+++ modules/components.py
@@ -31,7 +31,8 @@ def init_components(
 
     output = example_gen_pb2.Output(
         split_output = example_gen_pb2.SplitConfig(splits=[
-            example_gen_pb2.SplitConfig.Split(name='train',hash_buckets=8)
+            example_gen_pb2.SplitConfig.Split(name='train',hash_buckets=8),
+            example_gen_pb2.SplitConfig.Split(name='eval',hash_buckets=2)
         ])
     )
 
@@ -40,6 +41,37 @@ def init_components(
         output_config=output
     )
 
+    statistics_gen = StatisticGen(
+        examples=example_gen.outputs["examples"]
+    )
+
+    schema_gen = SchemaGen(
+        statistics=statistics_gen.outputs["statistics"]
+    )
+
+    example_validator = ExampleValidator(
+        statistics=statistics_gen.outputs["statistics"],
+        schema=schema_gen.outputs["schema"]
+    )
+
+    transform = Transform(
+        examples=example_gen.outputs["examples"],
+        schema=schema_gen.outputs["schema"],
+        module_file=os.path.abspath(transform_module)
+    )
+
+    trainer = Trainer(
+        module_file=os.path.abspath(training_module),
+        examples=transform.outputs["transformed_examples"],
+        transform_graph=transform.ouputs["transform_graph"],
+        schema=schema_gen.outputs["schema"],
+        train_args=trainer_pb2.TrainArgs(
+            splits=["train"],
+            num_steps=training_steps),
+        eval_args=trainer_pb2.EvalArgs(
+            splits=["eval"],
+            num_steps=eval_steps)
+    )
 
 
 
